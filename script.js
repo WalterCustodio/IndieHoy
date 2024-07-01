6f8982408fcf5f5db3fe67565c37fe2e7f64b8c4
WalterCustodio <82871842+WalterCustodio@users.noreply.github.com>
2024-07-01 10:28:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const carousels = document.querySelectorAll('.carousel');
+    const track = document.querySelector('.carousel-track');
+    const items = Array.from(track.children);
+    const nextButton = document.querySelector('.next');
+    const prevButton = document.querySelector('.prev');
+    
+    const itemWidth = items[0].getBoundingClientRect().width;
+    
+    let currentIndex = 0;
   
-    carousels.forEach(carousel => {
-      const track = carousel.querySelector('.carousel-track');
-      const items = Array.from(track.children);
-      const nextButton = carousel.querySelector('.next');
-      const prevButton = carousel.querySelector('.prev');
+    const setTrackPosition = (index) => {
+      track.style.transform = 'translateX(-' + itemWidth * index + 'px)';
+    };
   
-      const itemWidth = items[0].getBoundingClientRect().width + 20; // Considerar el margen
-      const totalItems = items.length;
-      let currentIndex = 0;
+    nextButton.addEventListener('click', () => {
+      if (currentIndex < items.length - 4) {
+        currentIndex++;
+        setTrackPosition(currentIndex);
+      }
+    });
   
-      const setTrackPosition = (index) => {
-        track.style.transform = 'translateX(-' + itemWidth * index + 'px)';
-      };
-  
-      nextButton.addEventListener('click', () => {
-        if (currentIndex < totalItems - 4) {
-          currentIndex++;
-          setTrackPosition(currentIndex);
-        }
-      });
-  
-      prevButton.addEventListener('click', () => {
-        if (currentIndex > 0) {
-          currentIndex--;
-          setTrackPosition(currentIndex);
-        }
-      });
+    prevButton.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        setTrackPosition(currentIndex);
+      }
     });
   });
   
