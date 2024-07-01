946e1130759560a9e3f927902160ce74e8ca65c2
BaccalaroRamiro <103059689+BaccalaroRamiro@users.noreply.github.com>
2024-07-01 06:02:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const track = document.querySelector('.carousel-track');
-    const items = Array.from(track.children);
-    const nextButton = document.querySelector('.next');
-    const prevButton = document.querySelector('.prev');
-    
-    const itemWidth = items[0].getBoundingClientRect().width;
-    
-    let currentIndex = 0;
+    const carousels = document.querySelectorAll('.carousel');
   
-    const setTrackPosition = (index) => {
-      track.style.transform = 'translateX(-' + itemWidth * index + 'px)';
-    };
+    carousels.forEach(carousel => {
+      const track = carousel.querySelector('.carousel-track');
+      const items = Array.from(track.children);
+      const nextButton = carousel.querySelector('.next');
+      const prevButton = carousel.querySelector('.prev');
   
-    nextButton.addEventListener('click', () => {
-      if (currentIndex < items.length - 4) {
-        currentIndex++;
-        setTrackPosition(currentIndex);
-      }
-    });
+      const itemWidth = items[0].getBoundingClientRect().width + 20; // Considerar el margen
+      const totalItems = items.length;
+      let currentIndex = 0;
   
-    prevButton.addEventListener('click', () => {
-      if (currentIndex > 0) {
-        currentIndex--;
-        setTrackPosition(currentIndex);
-      }
+      const setTrackPosition = (index) => {
+        track.style.transform = 'translateX(-' + itemWidth * index + 'px)';
+      };
+  
+      nextButton.addEventListener('click', () => {
+        if (currentIndex < totalItems - 4) {
+          currentIndex++;
+          setTrackPosition(currentIndex);
+        }
+      });
+  
+      prevButton.addEventListener('click', () => {
+        if (currentIndex > 0) {
+          currentIndex--;
+          setTrackPosition(currentIndex);
+        }
+      });
     });
   });
   
