aa8b8176be7f9908dd8445da742373b19f7051e9
WalterCustodio <82871842+WalterCustodio@users.noreply.github.com>
2024-07-01 19:30:58 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const track = document.querySelector('.carousel-track');
-    const items = Array.from(track.children);
-    const nextButton = document.querySelector('.next');
-    const prevButton = document.querySelector('.prev');
-    
-    const itemWidth = items[0].getBoundingClientRect().width;
-    
-    let currentIndex = 0;
-  
-    const setTrackPosition = (index) => {
-      track.style.transform = 'translateX(-' + itemWidth * index + 'px)';
-    };
-  
-    nextButton.addEventListener('click', () => {
-      if (currentIndex < items.length - 4) {
-        currentIndex++;
-        setTrackPosition(currentIndex);
-      }
+document.addEventListener("DOMContentLoaded", function () {
+  const track = document.querySelector(".carousel-track");
+  const items = Array.from(track.children);
+  const nextButton = document.querySelector(".next");
+  const prevButton = document.querySelector(".prev");
+
+  const itemWidth = items[0].getBoundingClientRect().width;
+
+  let currentIndex = 0;
+
+  const setTrackPosition = (index) => {
+    track.style.transform = "translateX(-" + itemWidth * index + "px)";
+  };
+
+  nextButton.addEventListener("click", () => {
+    if (currentIndex < items.length - 4) {
+      currentIndex++;
+      setTrackPosition(currentIndex);
+    }
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+      currentIndex--;
+      setTrackPosition(currentIndex);
+    }
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".evento-img");
+
+  images.forEach((img) => {
+    const hoverSrc = img.getAttribute("data-hover");
+    const originalSrc = img.getAttribute("data-original");
+
+    img.addEventListener("mouseover", () => {
+      img.src = hoverSrc;
     });
-  
-    prevButton.addEventListener('click', () => {
-      if (currentIndex > 0) {
-        currentIndex--;
-        setTrackPosition(currentIndex);
-      }
+
+    img.addEventListener("mouseout", () => {
+      img.src = originalSrc;
     });
   });
-  
+});
